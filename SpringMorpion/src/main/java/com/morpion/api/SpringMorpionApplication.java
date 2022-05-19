f2e8ed94bd5685d30dc1523a9647257b3f934584
Sarabac <36499355+Sarabac@users.noreply.github.com>
2022-05-19 16:46:15 +0200

    first

---
+++ SpringMorpion/src/main/java/com/morpion/api/SpringMorpionApplication.java
@@ -0,0 +1,13 @@
+package com.morpion.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMorpionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMorpionApplication.class, args);
+	}
+
+}
