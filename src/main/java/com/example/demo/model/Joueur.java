9958ed265de4b28e3e92da92c6191deff2a37b00
Sarabac <36499355+Sarabac@users.noreply.github.com>
2022-05-23 16:06:30 +0200

    ecrasement de la DB au debut

--- src/main/java/com/example/demo/model/Joueur.java
+++ src/main/java/com/example/demo/model/Joueur.java
@@ -2,13 +2,23 @@ package com.example.demo.model;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table
 @Getter
+@Setter
 public class Joueur {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    private String nom;
+
+    private String pwd;
+
+    private char marque;
+
+    private String couleur;
 }
